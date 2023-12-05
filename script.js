2f59fe2bb659141de87a4b2854218e54a0104390
Piotr Kowalczyk <piter2006602@wp.pl>
2023-12-06 00:02:35 +0100

    JavaScript-01

---
+++ script.js
@@ -0,0 +1,10 @@
+let dzialanie = ""
+function calculate(button){
+    dzialanie += button
+}
+
+function wynik(){
+    console.log(eval(dzialanie))
+    // return document.getElementById("display").innerText = eval(dzialanie)
+    
+}
