c3e7800fe4ee5be16cfb7b6d099efc9b90017650
Piotr Kowalczyk <piter2006602@wp.pl>
2023-12-06 23:43:32 +0100

    calculator

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let dzialanie = ""
-function calculate(button){
-    dzialanie += button
+const SPECIAL_CHARS = ["%", "*", "-", "+", "/"];
+let dzialanie = "";
+function calculate(button) {
+  if (
+    SPECIAL_CHARS.includes(button) &&
+    SPECIAL_CHARS.includes(
+      dzialanie[dzialanie.length - 1] && button !== "-" && button !== "+"
+    )
+  )
+    return;
+
+  dzialanie += button;
+  document.getElementById("display").innerText = dzialanie;
 }
 
-function wynik(){
-    console.log(eval(dzialanie))
-    // return document.getElementById("display").innerText = eval(dzialanie)
-    
+function wynik() {
+  dzialanie = eval(dzialanie);
+  return (document.getElementById("display").innerText =
+    parseFloat(dzialanie).toFixed(5));
+}
+
+function clearDisplay() {
+  dzialanie = "";
+  return (document.getElementById("display").innerText = " ");
+}
+
+function backspace() {
+  dzialanie = dzialanie.substring(0, dzialanie.length - 1);
+  return (document.getElementById("display").innerText = dzialanie);
 }
